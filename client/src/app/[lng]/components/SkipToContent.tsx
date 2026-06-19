ccce04d3023fd9ad621ef23227276d72946142ef
M11615 <M11615@outlook.com>
2026-06-19 16:48:09 +0800

    chore(deps): update all dependencies

--- client/src/app/[lng]/components/SkipToContent.tsx
+++ client/src/app/[lng]/components/SkipToContent.tsx
@@ -24,7 +24,9 @@ export default function SkipToContent(): React.ReactNode {
   if (!hydrated) return null;
   return (
     <button
-      onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => handleSkipToContent(e)}
+      onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
+        handleSkipToContent(e)
+      }}
       className="sr-only focus-visible:not-sr-only focus-visible:fixed focus-visible:left-[7px] focus-visible:top-[11px] focus-visible:flex focus-visible:items-center focus-visible:px-[13px] focus-visible:py-[6px] rounded-[6px] select-none cursor-pointer z-70 border-[var(--theme-bg-base)] bg-[var(--theme-bg-base)] text-[16px] text-[var(--theme-primary-light)] hover:text-[var(--theme-primary-light-hover)] transition duration-200 ease-in-out"
     >
       {t("skipToContent")}
